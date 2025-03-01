347addf4a7fa54484129a2b7966f1db02958b2ad
nallaperumal777 <nallaperumal.etl@gmail.com>
2025-03-01 06:11:47 +0000

    Docker's Spark Cluster workout

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,17 @@
 #/bin/bash
 
 echo `date`
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>"
 git status
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>"
 git add .
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>"
 git status
 git commit -m "Docker's Spark Cluster workout"
 echo "Git Branch"
 git branch
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>"
 git push -u origin main
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "Git Push completed at `date`"
 git status
